93e1e719282be794c3ff485c6af96ae8f839ea6c
Peng Du <rick.peng.du@gmail.com>
2020-08-19 21:16:54 -0700

    add shell

---
+++ src/MyMiniSearchEngine.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MyMiniSearchEngine {
+    // default solution. OK to change.
+    // do not change the signature of index()
+    private Map<String, List<List<Integer>>> indexes;
+
+    // disable default constructor
+    private MyMiniSearchEngine() {
+        indexes = new HashMap<>();
+        List<Integer> innerList = new ArrayList<>();
+        List<List<Integer>> outerList = new ArrayList<>();
+        outerList.add(innerList);
+        indexes.put("key", outerList);
+    }
+
+    public MyMiniSearchEngine(List<String> documents) {
+        index(documents);
+    }
+
+    // Each String in the List is considered a document.
+    // Assume documents only contain alphabetical words separated by white spaces.
+    private void index(List<String> texts) {
+        //homework
+    }
+
+    // search(key) return all the document ids where the given key phrase appears.
+    // key phrase can have one or two words in English alphabetic characters.
+    // return an empty list if search() finds no match in all documents.
+    public List<Integer> search(String keyPhrase) {
+        // homework
+        return new ArrayList<>(); // place holder
+    }
+}
